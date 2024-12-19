84bddaeda9ddea97eb487a0b7285734caa4cff9d
yo-yo0613 <chengyouli37@gmail.com>
2024-12-20 00:02:34 +0800

    Add files via upload

---
+++ java-tutorial/variables/main.java
@@ -0,0 +1,21 @@
+public class main {
+    
+    public static void main(String[] args) {
+        
+        int x = 123; // initialization
+        double y = 3.14;
+        boolean z= false;
+        char synbol = '@';
+        String name = "Hello, World!";
+
+        System.out.println("My number is: " + x);
+
+        System.out.println(z);
+
+        System.out.println(synbol);
+
+        System.out.println("Hello "+ name);
+
+        System.out.println("My decimal number is: " + y);
+    }
+}
